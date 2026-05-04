858e0c6cd04c8a211262a61962ccd531656bafb6
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-05-04 23:30:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=65bd476a-deb6-4585-87fb-7adbe34809e8",
     # 添加更多链接
 ]
 
